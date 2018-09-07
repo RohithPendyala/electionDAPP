5a012cf2ee274816cfc763479e9365745ff83f4c
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2018-09-07 14:30:21 +0530

    3. cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,6 +9,8 @@ contract Election {
 		uint voteCount;
 	}
 
+	// store accounts that have voted
+	mapping(address => bool) public voters;
 	// Store Candidates
 	// Fetch Candidate
 	mapping(uint => Candidate) public candidates;
@@ -26,6 +28,21 @@ contract Election {
 	function addCandidate(string _name) private {
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); 	
+	}
+
+	function vote(uint _candidateId) public {
+		// require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+
+		// record that voter(account) has voted
+		voters[msg.sender] = true;
+
+		// update candidate vote count
+		candidates[_candidateId].voteCount ++;
 	}
 
 
